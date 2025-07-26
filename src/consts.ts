a06edb2f3c690aa6955726d0f3fd548cd9efd9bc
DESKTOP-3KOP9PP\flo_p <flo_pekoe_psyche@yahoo.co.jp>
2025-07-26 17:22:20 +0900

    ロマサガ2リメイクのインデックスページをastroに移管

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const BASE_URL: string = function() {
-  let baseUrl = "https://taumax-develop.github.io/sagamax"
+  let baseUrl = "https://taumax-github.github.io/sagamax"
   if (import.meta.env.DEV) {
     baseUrl = "http://localhost:4321/sagamax"
   }
