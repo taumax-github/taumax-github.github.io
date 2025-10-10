a6f9aa18d8ff8bf5529f144a7df995b4bac21728
DESKTOP-3KOP9PP\flo_p <flo_pekoe_psyche@yahoo.co.jp>
2025-10-11 00:18:20 +0900

    scssへの移行

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,19 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://taumax-github.github.io',
-    // base: 'sagamax',
-    integrations: [mdx(), sitemap()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "./src/styles/_mixins.scss" as mixin;
+          @use "./src/styles/_vars.scss" as var;
+          `,
+        },
+      },
+    },
+  },
+  site: 'https://taumax-github.github.io',
+  // base: 'sagamax',
+  integrations: [mdx(), sitemap()],
 });
